b834d2128ff223b6b79b7915c1497d3f41d110d2
kannayya368 <kannayya.kr@gmail.com>
2020-07-21 19:13:09 +0530

    added files

---
+++ README.md
@@ -0,0 +1,4 @@
+"Welcome to Guessing Game of Files " 
+"21-07-2020" 
+
+---------- C:\PROGRAM FILES\GIT\GUESSGAME\GUESSINGGAME.SH: 32
---
+++ guessinggame.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Guessinggame Project
+#Author : Kannayya K Rathi
+#Date Created : 21-07-2020
+
+
+GuessingGame()
+{
+	true=1
+	while [[ $true  ]]; do
+		echo "enter number of files in the directory"
+		read no_of_lines
+		count=${ls -l | grep ^[\-] | wc -l}
+		if [[ $count -eq $no_of_lines ]]; then
+			echo "You guessed right Congratulations"
+			exit 0
+		elif [[ $count -lt $no_of_lines ]]; then
+			echo "You guessed less Try once more"
+		else
+			echo "You guessed more Try once more"
+		fi
+	done
+}
+
+echo "Welcome to Guessing Game"
+GuessingGame
+
+if [[ $? -eq 0 ]]; then
+	echo "Program exited successfully"
+fi
+		
